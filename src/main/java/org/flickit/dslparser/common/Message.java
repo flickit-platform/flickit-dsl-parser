3031c8ba93977816a3af3e106b626d9c9787a6f0
mahdirezazadeh <riza.rs2000@gmail.com>
2023-11-13 15:18:10 +0330

    DSLP-24:fix:Refactor

--- src/main/java/org/flickit/dslparser/common/Message.java
+++ src/main/java/org/flickit/dslparser/common/Message.java
@@ -1,8 +1,12 @@
 package org.flickit.dslparser.common;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Message {
 
-    public final static String PARSE_KIT_UNEXPECTED_ERROR_MESSAGE = "Unexpected error in parsing dsl to assessment kit";
-    public final static String PARSE_KIT_SYNTAX_ERROR_MESSAGE = "DSL has syntax error";
+    public static final String PARSE_KIT_UNEXPECTED_ERROR_MESSAGE = "Unexpected error in parsing dsl to assessment kit";
+    public static final String PARSE_KIT_SYNTAX_ERROR_MESSAGE = "DSL has syntax error";
     public static final String NOT_FOUND_FILE_NAME_MESSAGE = "NOT_DETERMINED";
 }
